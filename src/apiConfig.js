c551f68cbd95ae635c0a4c6ec92cc805e3086968
info <info@lmwlabs.faith>
2025-09-05 22:29:17 +0000

    Configure frontend to use production backend URL

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,3 +1,5 @@
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || '';
+const getApiUrl = () => {
+  return process.env.REACT_APP_API_URL || 'https://py-vectr-backend--vectr-dashboard.us-central1.hosted.app';
+};
 
-export default API_BASE_URL;
+export default getApiUrl;
